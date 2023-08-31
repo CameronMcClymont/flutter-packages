20c35524e1636228b321cc84a6dc7dcfdc5188fe
Cameron Duncan Mcclymont <cameron.duncan.mcclymont@cern.ch>
2023-08-31 21:54:25 +0200

    Add usage example for `platform` package

---
+++ packages/platform/example/README.md
@@ -0,0 +1,3 @@
+# platform_example
+
+An example app to show the basic usage of platform.
---
+++ packages/platform/example/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:platform/platform.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Platform Info Example',
+    home: PlatformInfoScreen(),
+  ));
+}
+
+class PlatformInfoScreen extends StatelessWidget {
+  final Platform platform = const LocalPlatform();
+
+  const PlatformInfoScreen({super.key});
+
+  String getPlatform() {
+    if (platform.isAndroid) {
+      return 'Android';
+    }
+    if (platform.isIOS) {
+      return 'iOS';
+    }
+    if (platform.isWindows) {
+      return 'Windows';
+    }
+    if (platform.isMacOS) {
+      return 'MacOS';
+    }
+    if (platform.isLinux) {
+      return 'Linux';
+    }
+
+    return 'Unknown';
+  }
+
+  String getOS() {
+    return kIsWeb ? 'web' : platform.operatingSystem;
+  }
+
+  String getExecutableDetails() {
+    return 'Executing: ${platform.executable}\nArgs: ${platform.executableArguments}';
+  }
+
+  String getFirstEnvVars() {
+    String envVars = '';
+
+    for (MapEntry envVar in platform.environment.entries.take(3)) {
+      envVars += '${envVar.key}: ${envVar.value}\n';
+    }
+
+    envVars += '...';
+    return envVars;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Platform Example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Hostname: ${platform.localHostname}\n'),
+                Text('Platform: ${getPlatform()} ${platform.version}\n'),
+                Text('OS: ${getOS()} | Processors: ${platform.numberOfProcessors}\n'),
+                Text('Currently executing `${platform.executable}` with args: ${platform.executableArguments}\n'),
+                Text('Environment variables:\n${getFirstEnvVars()}'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
